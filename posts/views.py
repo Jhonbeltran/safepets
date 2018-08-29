d96be886bc453b9db172fa1977f2b8e1d9fa1e7e
Jhonbeltran <jbeltranleon@gmail.com>
2018-08-29 00:29:12 -0500

    Agrego estructura base de templates

--- posts/views.py
+++ posts/views.py
@@ -1,43 +1,38 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 # Utilities
 from datetime import datetime
 
 posts = [
     {
-        'name': 'title',
-        'user': 'username',
-        'timestamp': datetime.now().strftime('%d/%m/%Y - %H:%M hours'),
-        'picture': 'https://picsum.photos/200/300?image=1083',
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name': 'title',
-        'user': 'username',
-        'timestamp': datetime.now().strftime('%d/%m/%Y - %H:%M hours'),
-        'picture': 'https://picsum.photos/200/300/?random',
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
-        'name': 'title',
-        'user': 'username',
-        'timestamp': datetime.now().strftime('%d/%m/%Y - %H:%M hours'),
-        'picture': 'https://picsum.photos/200/300?image=1080',
-    },
-    {
-        'name': 'title',
-        'user': 'username',
-        'timestamp': datetime.now().strftime('%d/%m/%Y - %H:%M hours'),
-        'picture': 'https://picsum.photos/200/300?image=1050',
-    },
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
+    }
 ]
 
 def list_posts(request):
-    """ Code generate like script CGI """
-    content = []
-    for post in posts:
-        #Desempaquetamos el diccionario usando **
-        content.append("""
-            <p><strong>{name}</strong></p>
-            <p><small>{user} - {timestamp}</small></p>
-            <figure><img src="{picture}"/></figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    """ List of existing posts """
+    # request, template, context
+    return render(request, 'feed.html', {'posts': posts})
