d8220d3fe7b63f5aabf8bd7a25782ab8266d64cf
Jhonbeltran <jbeltranleon@gmail.com>
2018-08-30 15:51:46 -0500

    Elimino el modelo de user y la base de datos

--- posts/models.py
+++ posts/models.py
@@ -1,20 +1,2 @@
 """ Post models """
 from django.db import models
-
-class User(models.Model):
-    """ Post User """
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    bio = models.TextField(blank=True)
-
-    is_admin = models.BooleanField(default=False)
-
-    birth_date = models.DateField(blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    modify = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.email
