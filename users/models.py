5aa6a27df7a2fab0b90a047098fb4e20d41fde02
Jhonbeltran <jbeltranleon@gmail.com>
2018-08-30 17:13:06 -0500

    Agrego requirements y aplicación de usuarios con un modelo proxy para profile

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    """ Profile model
+    Proxy model that extends User Default model"""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(max_length=200, blank=True)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    picture = models.ImageField(
+        upload_to='users/picture',
+        blank=True,
+        null=True
+    )
+
+    create = models.DateTimeField(auto_now_add = True)
+    modified = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
