8134b5bbf74e475084ba4ecf4cfaa931cacf846b
Jhonbeltran <jbeltranleon@gmail.com>
2018-08-29 22:41:21 -0500

    Agrego nuevo modelo usuario

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
+""" Post models """
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    """ Post Users """
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+
+    birth_date = models.DateField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modify = models.DateTimeField(auto_now=True)
