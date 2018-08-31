5ef35e65616336d15fc1a05f08d257a73d3aed92
Jhonbeltran <jbeltranleon@gmail.com>
2018-08-30 22:55:59 -0500

    registro app en admin usando class agrego atributos que se muestran, que son link y que se pueden editar sin ir a la vista por detalle

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from users.models import Profile
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """ Setup for the profile model Admin """
+
+    """Columns that the admin show"""
+    list_display = ('pk', 'user', 'phone_number','website', 'picture')
+    """Columns with link to detail view"""
+    list_display_links = ('pk', 'user',)
+    """Fields editables into de columns"""
+    list_editable = ('phone_number', 'website', 'picture')
