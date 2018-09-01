583d51c163bd98ca19d2779a78ccdcdd8e3282e0
Jhonbeltran <jbeltranleon@gmail.com>
2018-09-01 14:31:34 -0500

    Completo vista e implementación de login

--- users/views.py
+++ users/views.py
@@ -4,4 +4,14 @@ from django.contrib.auth import authenticate, login
 # Create your views here.
 def login_view(request):
     """ Login User """
+    if request.method == 'POST':
+        username = request.post['username']
+        password = request.post['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('feed')
+        else:
+            render(request, 'users/login.html',
+                   {'error', 'Username or Password are incorrect'})
     return render(request, 'users/login.html')
