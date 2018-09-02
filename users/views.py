aaa2af4fbd615d7abca7ad5e2765e9faeec6fd71
Jhonbeltran <jbeltranleon@gmail.com>
2018-09-01 22:24:52 -0500

    Agrego Registro de usuarios completo

--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth.models import User
+from users.models import Profile
+
 # Create your views here.
 def login_view(request):
     """ Login User """
@@ -19,6 +22,28 @@ def login_view(request):
 
 def signup(request):
     """ Sign Up Users """
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': "Passwords doesn't match"})
+
+        """ Here we create the user just with the username and password,
+            as we create in the basic admin """
+        user = User.objects.create_user(username=username, password=passwd)
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+
+        phone_number = request.POST['phone_number']
+
+        profile = Profile.objects.create(user=user, phone_number=phone_number)
+
+        return redirect('login')
+
     return render(request, 'users/signup.html')
 
 @login_required
