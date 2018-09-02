23e5b763e05bf685bbb749070b92eaaca754a918
Jhonbeltran <jbeltranleon@gmail.com>
2018-09-01 21:59:24 -0500

    Agrego base de Registro de usuarios

--- safepets/urls.py
+++ safepets/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>/', local_views.hi, name='hi'),
 
     path('posts/', posts_views.list_posts, name='feed'),
+    path('users/signup/', users_views.signup, name='signup'),
     path('users/login/', users_views.login_view, name='login'),
     path('users/logout/', users_views.logout_view, name='logout')
 
--- users/views.py
+++ users/views.py
@@ -17,6 +17,10 @@ def login_view(request):
 
     return render(request, 'users/login.html')
 
+def signup(request):
+    """ Sign Up Users """
+    return render(request, 'users/signup.html')
+
 @login_required
 def logout_view(request):
     logout(request)
