44429858d46250d7b8b8b9a94d61dc7cd005bc31
Jhonbeltran <jbeltranleon@gmail.com>
2018-09-02 16:42:41 -0500

    Agrego base de update profile

--- users/views.py
+++ users/views.py
@@ -12,7 +12,16 @@ from django.db.utils import IntegrityError
 
 def update_profile(request):
     """ Update a user's profile view """
-    return render(request, 'users/update_profile.html')
+    #request.user is provided for a middleware auth
+    profile = request.user.profile
+    return render(
+        request = request,
+        template_name= 'users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user
+        }
+    )
 
 # Create your views here.
 def login_view(request):
