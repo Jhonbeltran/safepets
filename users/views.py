cb10b253514113d2416cbba3b0b5186129332fb4
Jhonbeltran <jbeltranleon@gmail.com>
2018-09-03 23:20:34 -0500

    Creo agregar post url-view-template y puedo ver la imagen agregada en el template, agrego login required pendientes

--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ from django.db.utils import IntegrityError
 # Forms
 from users.forms import ProfileForm
 
-
+@login_required
 def update_profile(request):
     """ Update a user's profile view """
     #request.user is provided for a middleware auth
