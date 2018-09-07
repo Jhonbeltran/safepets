2e00b9ef2c897c12fdf28b41329abd0dbb3d843f
Jhonbeltran <jbeltranleon@gmail.com>
2018-09-07 09:26:52 -0500

    Agrego CreatePostView para crear posts usando una CBV

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render,redirect
 
 # Utilities
-from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 # Forms
 from posts.forms import PostForm
@@ -26,23 +26,15 @@ class PostDetailView(LoginRequiredMixin, DetailView):
     queryset = Post.objects.all()
     context_object_name = 'post'
 
-@login_required
-def create_post(request):
-    """ Create new post view """
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('posts:feed')
-    else:
-        form = PostForm()
+class CreatePostView(LoginRequiredMixin, CreateView):
+    """ Create a new post."""
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
 
-    return render(
-        request = request,
-        template_name = 'posts/new.html',
-        context = {
-            'form':form,
-            'user': request.user,
-            'profile': request.user.profile,
-            }
-        )
+    def get_context_data(self, **kwargs):
+        """ Ad user and profile to context. """
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['profile'] = self.request.user.profile
+        return context
